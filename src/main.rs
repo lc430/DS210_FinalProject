066ffd2b8dbce7ea627b87a7ab40ab749634a7bc
lc430 <113868052+lc430@users.noreply.github.com>
2023-05-07 23:02:42 -0400

    Add files via upload

--- src/main.rs
+++ src/main.rs
@@ -39,17 +39,33 @@ fn main() {
     let mut read_edges = read_file::read_file("facebook_combined.txt");
     let mut graph = graph::Graph::create_directed(4039, &read_edges);
     let mut result: Vec<i32> = vec![];
+    let total_dis: usize = 88234;
+    let total_average_dis: f64 = total_dis as f64/4039 as f64;
+    let mut count_node = vec![];
+    let mut last_node = vec![];
     
     for i in 0..4039 {
-        result.push(bfs::bfs_computation(i, &graph));             
-    }
-    
+        result.push(bfs::bfs_computation(i, &graph));   
+    }   
     println!("Total of 4039 nodes, 88234 edges: ");
     println!("The average distance from nodes to all other nodes connected is: ");
-    
     println!("{:?}",result);
     println!("From statistics aspect: {:?}", count_degree(result));
+
+    for (i, l) in graph.outedges.iter().enumerate(){
+        count_node.push(l.len());
+        last_node.push(i);
+    }
+    count_node.sort();
+    last_node.sort();
     
+    let min_value: usize = *count_node.iter().min().unwrap();
+    let max_value: usize = *count_node.iter().max().unwrap();
+    
+    println!("Last Node: {}", last_node[last_node.len()-1]);
+    println!("Min Connections: {}", min_value);
+    println!("Max Connections: {}", max_value);
+    println!("Total Average Distance(Wrong, just for reference): {}", total_average_dis); 
 }
 
 
